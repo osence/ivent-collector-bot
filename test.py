1e73bd9ca5ceb340fe2580f3a562468ba2b5e8df
osence <tri29092000@mail.ru>
2021-11-14 23:36:23 +0300

    add new function in test.py

--- test.py
+++ test.py
@@ -1,71 +1,97 @@
 def getAllIvents():
     # code
-    return list(list(id, name))     #sort by ascending date
+    return list(list(id, name))  # sort by ascending date
+
 
 def searchLocation(coords):
     # code
-    return list(list(id, name))     #sort by ascending date
+    return list(list(id, name))  # sort by ascending date
+
 
 def searchDate(dateStart, dateEnd):
     # code
-    return list(list(id, name))     #sort by ascending date
+    return list(list(id, name))  # sort by ascending date
+
 
 def searchThematic(thema):
     # code
-    return list(list(id, name))     #sort by ascending date
+    return list(list(id, name))  # sort by ascending date
 
-def searchPay(payMode):
+
+def searchPay(minCost, maxCost):
     # code
-    return list(list(id, name))     #sort by ascending date
+    return list(list(id, name))  # sort by ascending date
+
 
 def userRegistration(id, fio, dataBirthday):
     # code
-    return bool     #True or False (result of inserting in DB)
+    return bool  # True or False (result of inserting in DB)
+
 
 def userEditInfo(id, fio, dataBirthday):
     # code
-    return bool     #True or False (result of inserting in DB)  
-    
+    return bool  # True or False (result of inserting in DB)  
+
+
 def userGetInfo(id):
     # code
     return list(fio, dataBirthday)
 
-def newIvent(iventId, iventName, description, date, time, place, subject, payMode, numberOfSeats, userId ):
-    # code
-    return bool     #True or False (result of inserting in DB) 
 
-def editIventInfo(iventId, iventName, description, date, time, place, subject, payMode, numberOfSeats, userId ):
+def newIvent(iventId, iventName, description, date, time, place, subject, payMode, numberOfSeats, userId):
     # code
-    return bool     #True or False (result of inserting in DB) 
-    
+    return bool  # True or False (result of inserting in DB) 
+
+
+def editIventInfo(iventId, iventName, description, date, time, place, subject, payMode, numberOfSeats, userId):
+    # code
+    return bool  # True or False (result of inserting in DB) 
+
+
 def removeIvent(iventId):
-    #code
-    return bool     #True or False (result of inserting in DB) 
-    
+    # code
+    return bool  # True or False (result of inserting in DB) 
+
+
 def getIventInfo(iventId):
     # code
     return list(iventId, iventName, description, date, time, place, subject, payMode, numberOfSeats, userId)
 
+
 def userAddIventRegistration(userId, iventId):
     # code
-    return bool      # True or False (result of inserting in DB) 
+    return bool  # True or False (result of inserting in DB) 
+
 
 def userRemoveIventRegistration(userId, iventId):
     # code
-    return bool     # True or False (result of inserting in DB) 
+    return bool  # True or False (result of inserting in DB) 
+
 
 def userGetIventRegistrations(userId):
     # code
-    return list(iventId, iventName)     #sort by ascending date
+    return list(iventId, iventName)  # sort by ascending date
 
-def getMapPlace(place):
+
+def showIventsAddedByUser(userId):
     # code
-    return link     #link to map
-    
+    return list(iventId, iventName)  # sort by ascending date
+
+
+def getMapFromCoords(coords):
+    # code
+    return link  # link to map
+
+
+def getMapFromAdress(adress):
+    # code
+    return link  # link to map    
+
+
 def handler(userRequest):
     if (userRequest == 'getAllIvents'):
         getAllIvents()
     elif (userRequest == 'searchLocation')
         searchLocation()
-        #...
-    return bool     # True or False (return of the called function) 
+        # ...
+    return bool  # True or False (return of the called function) 
